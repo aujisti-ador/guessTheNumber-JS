2b887e7818c095923d9d1a538df07aa0deb92cb3
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2021-11-19 00:52:31 +0600

    again button function added, functions renamed, bug fixed

--- script.js
+++ script.js
@@ -2,19 +2,45 @@ let randomNumber = Math.floor(Math.random() * 20) + 1
 let score = 20
 let highScore = 0
 // document.querySelector('.number').textContent = randomNumber
-console.warn(randomNumber)
+// console.warn(randomNumber)
 // console.log(document.querySelector('.guess').value = '30')
 
+
+// Set again button
+const setAgainButton = function () {
+    randomNumber = Math.floor(Math.random() * 20) + 1
+    // console.warn(randomNumber)
+    score = 20
+    document.querySelector('.score').textContent = score
+    document.querySelector('.message').textContent = 'Start guessing...!'
+    document.querySelector('.number').style.width = '15rem'
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.guess').value = ''
+}
+
 // when number matched to random number
-const numberMatchedUiChange = function () {
+const setNumberMatchedUiChange = function () {
     document.querySelector('.message').textContent = '🥂 Correct Number...!'
     document.querySelector('.number').textContent = randomNumber
     document.querySelector('.number').style.width = '25rem'
     document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.score').textContent = score
+    document.querySelector('.highscore').textContent = highScore
 }
 
 // when number doesn't match to random number
-const numberDoesntMatchedUiChange = function () {
+const setNumberDoesntMatchUiChange = function (guessedNumber) {
+    score--
+    guessedNumber > randomNumber ? document.querySelector('.message').textContent = '📈 Too High!' : document.querySelector('.message').textContent = '📉 Too Low!'
+    document.querySelector('.score').textContent = score
+    // document.querySelector('.message').textContent = 'Start guessing...!'
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').textContent = '?'
+}
+
+// when input is empty value
+const setEmptyUiChange = function () {
     document.querySelector('.message').textContent = '😒 Empty Number...!'
     document.querySelector('body').style.backgroundColor = '#8B0000'
     document.querySelector('.number').textContent = '😑'
@@ -24,16 +50,18 @@ const numberDoesntMatchedUiChange = function () {
 const isNumberMatched = function (guessedNumber) {
     // when number matched to random number
     if (guessedNumber === randomNumber) {
-        numberMatchedUiChange()
+        score > highScore ? highScore = score : ''
+        setNumberMatchedUiChange()
         // when number doesn't match and not empty value
     } else if (guessedNumber != '') {
-        // console.log(guessedNumber)
-        guessedNumber > randomNumber ? document.querySelector('.message').textContent = '📈 Too High!' : document.querySelector('.message').textContent = '📉 Too Low!'
+        setNumberDoesntMatchUiChange(guessedNumber)
         // when input is empty but pressed check buton
     } else {
         // when number doesn't match to random number
-        numberDoesntMatchedUiChange()
+        setEmptyUiChange()
     }
+
+
 }
 
 
@@ -46,3 +74,4 @@ const handleButtonPressCheck = function () {
 
 // Handle 'Check!' button
 document.querySelector('.check').addEventListener('click', handleButtonPressCheck)
+document.querySelector('.again').addEventListener('click', setAgainButton)
