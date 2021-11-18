cf023e0edb4649bde977420e28ffd16a0e752801
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2021-11-19 00:02:35 +0600

    isNumberMatched function refactor

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let randomNumber = Math.floor(Math.random() * 20) + 1
+let score = 20
+let highScore = 0
 // document.querySelector('.number').textContent = randomNumber
 console.warn(randomNumber)
 // console.log(document.querySelector('.guess').value = '30')
 
-
+// when number matched to random number
 const numberMatchedUiChange = function () {
     document.querySelector('.message').textContent = '🥂 Correct Number...!'
     document.querySelector('.number').textContent = randomNumber
@@ -11,10 +13,26 @@ const numberMatchedUiChange = function () {
     document.querySelector('body').style.backgroundColor = '#60b347'
 }
 
+// when number doesn't match to random number
+const numberDoesntMatchedUiChange = function () {
+    document.querySelector('.message').textContent = '😒 Empty Number...!'
+    document.querySelector('body').style.backgroundColor = '#8B0000'
+    document.querySelector('.number').textContent = '😑'
+}
+
 // Check if the Guessed number is equal to Random Number
-const doesNumberMatch = function (guessedNumber) {
+const isNumberMatched = function (guessedNumber) {
+    // when number matched to random number
     if (guessedNumber === randomNumber) {
         numberMatchedUiChange()
+        // when number doesn't match and not empty value
+    } else if (guessedNumber != '') {
+        // console.log(guessedNumber)
+        guessedNumber > randomNumber ? document.querySelector('.message').textContent = '📈 Too High!' : document.querySelector('.message').textContent = '📉 Too Low!'
+        // when input is empty but pressed check buton
+    } else {
+        // when number doesn't match to random number
+        numberDoesntMatchedUiChange()
     }
 }
 
@@ -22,7 +40,7 @@ const doesNumberMatch = function (guessedNumber) {
 // 'check!' button Handler
 const handleButtonPressCheck = function () {
     let guessedNumber = Number(document.querySelector('.guess').value)
-    doesNumberMatch(guessedNumber)
+    isNumberMatched(guessedNumber)
     // console.warn(typeof (guessedNumber))
 }
 
